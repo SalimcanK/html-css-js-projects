8d45c396fe340b823e63bdba07085dbe0a1964ff
SalimcanK <70902216+SalimcanK@users.noreply.github.com>
2023-09-27 12:27:40 +0300

    Add files via upload

--- 25-) blurry-loading/script.js
+++ 25-) blurry-loading/script.js
@@ -6,13 +6,14 @@ let load = 0;
 let int = setInterval(blurring, 30);
 
 function blurring() {
-  load = load + 1.4;
+  load = load + 1.8;
 
   if (load > 99) {
     clearInterval(int);
   }
 
-  loadText.innerText = `${Math.floor(load)}%`;
+  
+  loadText.innerText = `${load.toFixed(0)}%`;
   loadText.style.opacity = scale(load, 0, 100, 1, 0); // Our load value goes to 0-100 but opacity value can be between 0-1 so we have to scale it
   bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
 }
